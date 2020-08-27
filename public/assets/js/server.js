9b22ac1dfd81c3440769da65227cfd76dd9debf6
Edward Reyes <reyes.edwardf@gmail.com>
2020-08-27 00:27:23 -0700

    created routes for index & css files and was able to retrieve POST data

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -4,14 +4,45 @@ var app = express();
 var path = require("path");
 
 const port = 3000;
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/notes', function(req, res) {
+app.use(express.static(__dirname));
+// GET /notes route to return notes.html file
+app.get('/notes', function (req, res) {
     console.log(__dirname);
     res.sendFile(path.join(__dirname, "../../../public/notes.html"));
+    console.log()
 })
 
-app.get('/', function(req, res) {
+// GET / route to return index.html file
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, "../../../public/index.html"));
 })
 
+// GET route to index.js
+app.get('/assets/js/index.js', function (req, res) {
+    res.sendFile(path.join(__dirname, "index.js"));
+})
+
+// Get route to css
+app.get('/assets/css/styles.css', function (req, res) {
+    res.sendFile(path.join(__dirname, "../css/styles.css"));
+})
+
+var notes = [
+
+]
+
+app.get("/api/notes", function (req, res) {
+    return res.json(notes);
+});
+
+// Create a new note
+app.post("/api/notes", function (req, res) {
+    var newNote = req.body;
+    notes.push(newNote);
+    res.json(newNote);
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
