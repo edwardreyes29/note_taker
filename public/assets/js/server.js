05c2484c126b49b92f1a4bc48c34bd7583cb0aaa
Edward Reyes <reyes.edwardf@gmail.com>
2020-08-26 23:25:20 -0700

    added package script to run server and route / to send the index.html page

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+
 var app = express();
 var path = require("path");
-const port = 3000;
 
+const port = 3000;
 
 app.get('/notes', function(req, res) {
     console.log(__dirname);
     res.sendFile(path.join(__dirname, "../../../public/notes.html"));
+})
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "../../../public/index.html"));
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
