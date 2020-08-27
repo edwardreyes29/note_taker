59a39bdf64b42414aced1ca1d7f80497277de2eb
Edward Reyes <reyes.edwardf@gmail.com>
2020-08-27 03:44:57 -0700

    cleaned up code and changed port for heroku deployment

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Unit 11 Express Homework: Note Taker
 
+## To run
+npm run start
+
 ## Description
 
 Create an application that can be used to write, save, and delete notes. This application will use an express backend and save and retrieve note data from a JSON file.
--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -4,12 +4,12 @@ const fs = require('fs');
 var app = express();
 var path = require("path");
 
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-app.use(express.static(__dirname));
 // GET /notes route to return notes.html file
 app.get('/notes', function (req, res) {
     console.log(__dirname);
@@ -22,64 +22,53 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, "../../../public/index.html"));
 })
 
-// GET route to index.js
-app.get('/assets/js/index.js', function (req, res) {
-    res.sendFile(path.join(__dirname, "index.js"));
-})
-
-// Get route to css
-app.get('/assets/css/styles.css', function (req, res) {
-    res.sendFile(path.join(__dirname, "../css/styles.css"));
-})
-
 // Sends JSON data as a response
 app.get("/api/notes", function (req, res) {
-    let rawdata =  getJSONData(); 
-    let notes = JSON.parse(rawdata);
-    res.json(notes);
+    const notes = JSON.parse(getJSONData()); // parse data to JSON
+    res.json(notes); // send response
 });
 
 // Create a new note, assign a unique id, and store it into the db.json file
 app.post("/api/notes", function (req, res) {
-    let rawdata =  getJSONData(); // get raw data
-    let notes = JSON.parse(rawdata); // parse data to JSON
-    var newNote = req.body; // get new note
-    notes.push(newNote); // push to array
-    assignIDs(notes); // assign unique id's to each item
-    writeToJSON(notes);
-    res.json(newNote);
+    const notes = JSON.parse(getJSONData()); // parse data to JSON
+    const newNote = req.body;   // get new note
+    notes.push(newNote); // push new note to notes array
+    const assignedNotes = assignUniqueIds(notes); // assign unique id's to each item and return a new array
+    writeToJSON(assignedNotes); // store new notes to db.json
+    res.json(true); // send response
 });
 
-// Delete note
+// Delete a note
 app.delete("/api/notes/:id", function(req, res) {
-    console.log(req.params.id);
-    let id = parseInt(req.params.id);
-    let rawdata =  getJSONData(); 
-    let notes = JSON.parse(rawdata);
-    const filteredNotes = notes.filter(note => parseInt(note.id) !== id);
-    writeToJSON(filteredNotes);
-    res.json(filteredNotes);
+    const chosenId = parseInt(req.params.id); // Get the id of the selected note for deletion
+    const notes = JSON.parse(getJSONData()); // parse data to JSON
+    const filteredNotes = notes.filter(note => parseInt(note.id) !== chosenId); // filters array with matching id and returns a new filtered array
+    writeToJSON(filteredNotes); // store new notes to db.json
+    res.json(filteredNotes); // send response
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
 
-// File System
+// Stores notes to db.json
 const writeToJSON = (notes) => {
     let data = JSON.stringify(notes, null, 4);
     fs.writeFile(path.join(__dirname, "../../../db/db.json"), data, (err) => {
         if (err) throw err;
-        console.log('The note has been saved!');
     });
 }
 
+// Returns data from db.json
 const getJSONData = () => {
     return fs.readFileSync(path.join(__dirname, "../../../db/db.json"));
 }
 
-const assignIDs = array => {
-    console.log(array)
-    for (let i = 0; i < array.length; i++) {
-        array[i].id = i+1;
+// Assigns a unique id for each note
+const assignUniqueIds = array => {
+    const copyArray = [...array];
+    for (let i = 0; i < copyArray.length; i++) {
+        copyArray[i].id = i+1;
     }
-    return array;
+    return copyArray;
 }
